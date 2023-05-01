a20c3b82efa3d96ca92e2c9973f65628308e017d
Rafe7350 <117887198+Rafe7350@users.noreply.github.com>
2023-05-01 09:53:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,4 @@
 //your JS code here. If required.
-document.write("DOM load success");
+// document.write("DOM load success");
+document.addEventListener("DOMContentLoaded", function() {
+        document.body.innerHTML = "DOM load success";
